ea076a13af5c324351095a3394753f9a5ab7920c
Debashis Moharana <debashismoharana@users.noreply.github.com>
2019-08-02 20:21:36 +0530

    show github profiles on search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GithubService } from './shared/github.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'coditas-angular-assignment';
+  searchString: string;
+  profiles: any;
+  currentPageNumber = 1;
+  profilesToShow = [];
+  pages = [];
+  constructor(private githubService: GithubService) { }
+
+  showResultOnKeyStroke() {
+    this.githubService.searchString(this.searchString).subscribe((response) => {
+      this.profiles = response;
+      this.pages = [];
+      for (let i = 1; i <= this.profiles['items'].length / 10; i++) {
+        this.pages.push(i);
+      }
+      this.profilesToShow = this.profiles['items'].slice(0, 10);
+      console.log(this.profiles);
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
+  sortResult(value: string) {
+    console.log(value);
+    switch (value) {
+      case 'sortByName':
+        this.showResultOnKeyStroke();
+        this.profiles['items'].sort((a, b) => a.login - b.login);
+        break;
+      case 'sortByRank':
+        this.profiles['items'].sort((a, b) => a.score - b.score);
+        break;
+    }
+    this.goToPage(this.currentPageNumber);
+  }
+  goToPage(pageNumber) {
+    this.profilesToShow = this.profiles['items'].slice(pageNumber * 10 - 9, pageNumber * 10);
+  }
 }
---
+++ src/app/shared/github.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  constructor(private http: HttpClient) { }
+
+  searchString(searchString) {
+    return this.http.get(`https://api.github.com/search/users?q=${searchString}`);
+  }
+}
